9e18e30e0dc31feac5937f82ac6937f2affff4b1
ethandu <ethan_du@sjtu.edu.cn>
2025-01-30 11:49:48 +0100

    drawmap.js

---
+++ static/script/drawmap.js
@@ -0,0 +1,51 @@
+var canvas;
+var ctx;
+var tileSize = 50;
+var FPS = 50;
+
+var map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
+
+function init() {
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    drawMap();
+}
+
+function drawMap() {
+    for (var row = 0; row < map.length; row++) {
+        for (var col = 0; col < map[row].length; col++) {
+            var tile = map[row][col];
+            drawTile(ctx, col, row, tile);
+        }
+    }
+}
+
+function drawTile(ctx, col, row, tile) {
+    var x = col * tileSize;
+    var y = row * tileSize;
+
+    switch (tile) {
+        case 0:
+            ctx.fillStyle = 'white'; // Empty space
+            break;
+        case 1:
+            ctx.fillStyle = 'red'; // Wall
+            break;
+    }
+
+    ctx.fillRect(x, y, tileSize, tileSize);
+}
+
+// Call the init function when the window loads
+window.onload = init;
