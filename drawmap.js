241dc68a009e872e7395c87b6433ff4e9588e11e
Horizon-cn <154842645@qq.com>
2025-01-30 19:06:11 +0100

    renew the game: rounds and beans

--- drawmap.js
+++ drawmap.js
@@ -9,11 +9,14 @@ var pacmanImage = new Image();
 pacmanImage.src = 'bluetiger.png';
 var refreshInterval = 1000; // Refresh interval in milliseconds
 var wallsToRemove = 10;
-var wallsToAdd = 10;
+var wallsToAdd = 11;
 var wallDensity = 0.85;
+var beannum = 10;
 var ghostImage = new Image();
 ghostImage.src = 'manuel.png'; // Assuming you have a ghost image named 'ghost.png'
 var ghostMoveInterval = 200; // Interval for ghost movement in milliseconds
+
+var round = 1; // 定义回合计数
 
 var ghosts = [
     { x: 12, y: 1 },
@@ -24,13 +27,17 @@ var ghosts = [
     { x: 13, y: 10 }
 ];
 
+var beans = [];
+
 function generateRandomMap(rows, cols, probabilityOfZero) {
     var map = [];
     for (var row = 0; row < rows; row++) {
         var rowArray = [];
         for (var col = 0; col < cols; col++) {
             // Generate a random number and compare it with the probability
-            if (Math.random() < probabilityOfZero) {
+            if(pacman.x === col && pacman.y === row){
+                rowArray.push(0);
+            }else if (Math.random() < probabilityOfZero) {
                 rowArray.push(0);
             } else {
                 rowArray.push(1);
@@ -65,6 +72,8 @@ function init() {
     drawMap();
     drawPacman();
     drawGhosts();
+    generateBeans();
+    drawBeans();
 
     window.addEventListener('keydown', movePacman);
     setInterval(refreshMap, refreshInterval);
@@ -106,6 +115,28 @@ function drawGhosts() {
     });
 }
 
+// 随机生成25个豆子的位置
+function generateBeans() {
+    while (beans.length < beannum) {
+        var x = Math.floor(Math.random() * (map[0].length - 2)) + 1;
+        var y = Math.floor(Math.random() * (map.length - 2)) + 1;
+        if (map[y][x] === 0 && !beans.some(bean => bean.x === x && bean.y === y)) {
+            beans.push({ x: x, y: y });
+        }
+    }
+}
+
+// 绘制豆子
+function drawBeans() {
+    beans.forEach(function(bean) {
+        ctx.beginPath();
+        ctx.arc((bean.x + 0.5) * tileSize, (bean.y + 0.5) * tileSize, tileSize / 15, 0, 2 * Math.PI);
+        ctx.fillStyle = 'blue';
+        ctx.fill();
+    });
+}
+
+
 function movePacman(event) {
     var newX = pacman.x;
     var newY = pacman.y;
@@ -139,11 +170,26 @@ function movePacman(event) {
         drawGhosts();
         resetGame();
     } else {
+        beans = beans.filter(function(bean) {
+            return !(bean.x === pacman.x && bean.y === pacman.y);
+        });
+        
+        if (beans.length === 0) {
+            alert("Next Round");
+            ghostMoveInterval *= 0.9; // 提升10%
+            beannum += 5;
+            map = generateRandomMap(height / tileSize, width / tileSize, wallDensity);
+            generateBeans(beannum);
+            round++; // 增加回合计数
+            document.getElementById('round-counter').innerText = `回合: ${round}`; // 更新显示的回合计数
+        }
+
         // Redraw the map, Pac-Man, and ghosts
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         drawMap();
         drawPacman();
         drawGhosts();
+        drawBeans();
     }
 }
 
@@ -176,6 +222,7 @@ function moveGhosts() {
         drawMap();
         drawPacman();
         drawGhosts();
+        drawBeans();
     }
 }
 
@@ -199,18 +246,39 @@ function refreshMap() {
     // Add certain number of walls
     for (var i = 0; i < wallsToAdd; i++) {
         var row, col;
+        var isValidPosition;
         do {
             row = Math.floor(Math.random() * (map.length - 2)) + 1;
             col = Math.floor(Math.random() * (map[0].length - 2)) + 1;
-        } while (map[row][col] !== 0 || (row === pacman.y && col === pacman.x));
+
+            // 检查新生成的墙是否与ghost的位置重合
+            isValidPosition = true;
+            for (var j = 0; j < ghosts.length; j++) {
+                if (ghosts[j].x === col && ghosts[j].y === row) {
+                    isValidPosition = false;
+                    break;
+                }
+            }
+            for (var k = 0; k < beans.length; k++) {
+                if (beans[k].x === col && beans[k].y === row) {
+                    isValidPosition = false;
+                    break;
+                }
+            }
+        } while (map[row][col] !== 0 || (row === pacman.y && col === pacman.x) || !isValidPosition);
         map[row][col] = 1;
     }
 
     // Redraw the map, Pac-Man, and ghosts
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    round = 1; // 重置回合计数
+    document.getElementById('round-counter').innerText = `回合: ${round}`; // 更新显示的回合计数
+
     drawMap();
     drawPacman();
     drawGhosts();
+    drawBeans();
 }
 
 function resetGame() {
@@ -224,9 +292,14 @@ function resetGame() {
         { x: 13, y: 10 }
     ]; // Reset ghosts' positions
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    map = generateRandomMap(height / tileSize, width / tileSize, wallDensity);
+    beannum = 10;
+    ghostMoveInterval = 200;
     drawMap();
     drawPacman();
     drawGhosts();
+    generateBeans(beannum);
+    drawBeans();
 }
 
 // Start the game when the "Start" button is clicked
